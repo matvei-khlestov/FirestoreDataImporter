108f25dc6b9138cf3a05b8b208297ad218e2743e
Matvei Khlestov <khlestov.matvei@mail.ru>
2026-01-06 07:10:59 +0300

    chore: delete Main.storyboard

---
+++ FirestoreDataImporter/FirestoreDataImporter/App/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  FirestoreDataImporter
+//
+//  Created by Matvei Khlestov on 06.01.2026.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ FirestoreDataImporter/FirestoreDataImporter/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FirestoreDataImporter
+//
+//  Created by Matvei Khlestov on 06.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
+
